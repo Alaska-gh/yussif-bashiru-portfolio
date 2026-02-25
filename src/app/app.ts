110a7960a9e0280356ce10bff2ff947f359b58cb
Alaska-gh <bashiruyussif@gmail.com>
2026-02-25 16:31:57 +0000

    create skils, projects and contact sections and adjust paddings for the sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
+import { Projects } from './features/projects/projects';
+import { Skills } from './features/skills/skills';
 import { Component, signal } from '@angular/core';
 import { Hero } from './features/hero/hero';
 import { Header } from './shared/header/header';
 import { About } from "./features/about/about";
+import { Contact } from './features/contact/contact';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [Hero, Header, About],
+  imports: [Hero, Header, About, Skills, Projects, Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
